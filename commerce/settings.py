2c2b267b18bbdc0880d327482668868751213910
Kb-Jr <abdulkabiruj@gmail.com>
2022-11-03 15:24:17 +0100

    Update settings.py
    Removed secret key value

--- commerce/settings.py
+++ commerce/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-kp#yqmbh-5ai2$d(i02%4+(@p^^b^i8obqa-pkaxx4@i8p1sd'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
